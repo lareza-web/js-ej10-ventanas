5fecea63ae0738264124eccf8d69094f77e248be
lareaza-web <larezaweb@gmail.com>
2020-05-18 17:26:36 -0300

    ventana

---
+++ ventanas.js
@@ -0,0 +1,18 @@
+'use strict'
+/*
+//ALERTA
+alert( 'Esta es mi alerta');
+alert(' Este es mi texto');
+*/
+
+/*
+// confirmacion
+
+var mi_resultado = confirm("Estas seguro de querer continuar");
+console.log(mi_resultado);
+*/
+
+// Ingreso Datos
+
+var mi_resultado = prompt("Que edad tienes", 18);
+console.log(mi_resultado);
